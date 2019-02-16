cfd70617123cbd61886266299aac757f9b45b9dc
Unknown <C0zz4a@hotmail.co.uk>
2019-02-16 18:48:43 +0000

    Homeless boi in the house
    Still needs movement adjustments

---
+++ Assets/Scripts/Player Controller/PlayerAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            anim.SetBool("isCrouching", true);
+        } else
+        {
+            anim.SetBool("isCrouching", false);
+        }
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetBool("isWalkingBack", false);
+                anim.SetBool("isWalking", true);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isWalkingBack", true);
+            }
+        } else
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isWalkingBack", false);
+        }
+        	
+	}
+}
