c5ea857781324db0ca3b1caebacc48a5dd756616
lacia9419 <46899186+lacia9419@users.noreply.github.com>
2019-02-17 14:22:18 +0000

    Added kitchen props

---
+++ Assets/FridgeDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeDoor : Interactable
+{
+    public bool IsOpen;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void PlayerInteract()
+    {
+        if (IsOpen) transform.Rotate(0, 0, 90, Space.Self);
+        else transform.Rotate(0, 0, -90, Space.Self);
+        IsOpen = !IsOpen;
+    }
+}
