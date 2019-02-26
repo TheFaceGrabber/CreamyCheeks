b52ca0e8524107938e7ecb4c4578955dc2a2f115
lacia9419 <46899186+lacia9419@users.noreply.github.com>
2019-02-26 22:13:58 +0000

    Minor UI Update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,13 +6,24 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour {
     private GameObject InteractText;
     private GameObject StatsPanel;
+    private Image Healthbar;
+    private Image Sanitybar;
+    private Text HealthText;
+    private Text SanityText;
+    private float barstartsize;
+    
 	// Use this for initialization
 	void Start () {
         InteractText = transform.GetChild(3).gameObject;
         StatsPanel = transform.GetChild(1).gameObject;
-
-
+        Healthbar = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Image>();
+        HealthText = transform.GetChild(0).GetChild(0).GetChild(1).GetComponent<Text>();
+        Sanitybar = transform.GetChild(0).GetChild(1).GetChild(0).GetComponent<Image>();
+        SanityText = transform.GetChild(0).GetChild(1).GetChild(1).GetComponent<Text>();
+        barstartsize = Healthbar.transform.localScale.x;
+        
         HideInteractText();
+      
 	}
 
     // Update is called once per frame
@@ -72,4 +83,19 @@ public class UIManager : MonoBehaviour {
             }
         }
     }
+
+    public void UpdateHealth(int newhealth)
+    {
+        HealthText.text = "Health: " + newhealth + "/10";
+        Healthbar.transform.localScale = new Vector2(((float)newhealth / 10) * barstartsize, transform.localScale.y);
+    }
+
+    public void UpdateSanity(int newsanity)
+    {
+        SanityText.text = "Sanity: " + newsanity + "/10";
+        Sanitybar.transform.localScale = new Vector2(((float)newsanity / 10) * barstartsize, transform.localScale.y);
+        
+    }
+
+    
 }
