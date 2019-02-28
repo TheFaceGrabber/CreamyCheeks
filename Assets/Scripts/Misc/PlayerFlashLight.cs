107b25ad0e953a69c6ee35195f4fa9ef7897c6a9
Joe <Joe>
2019-02-28 17:31:05 +0000

    Added Player Flashlight + Fixed AI speedy boi bug

---
+++ Assets/Scripts/Misc/PlayerFlashLight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreamyCheaks.Input;
+
+public class PlayerFlashLight : MonoBehaviour {
+
+    public Item RequiredItem;
+
+    InventorySystem inv;
+    Light light;
+    
+    private void Start()
+    {
+        inv = GameObject.Find("Inventory").GetComponent<InventorySystem>();
+        light = GetComponent<Light>();
+    }
+
+    void Update ()
+    {
+		if(InputManager.GetButtonDown("Toggle Flashlight"))
+        {
+            if(inv.CheckForItem(RequiredItem))
+            {
+                light.enabled = !light.enabled;
+            }
+        }
+	}
+}
