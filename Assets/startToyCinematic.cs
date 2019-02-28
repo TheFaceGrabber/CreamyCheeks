cf3389f10b148ac1e27d57829a5817b2e9443e46
Unknown <C0zz4a@hotmail.co.uk>
2019-02-28 19:20:47 +0000

    Started Toy Room Cinematic
    timeline is there just hasn't got anything to initiate.

---
+++ Assets/startToyCinematic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startToyCinematic : Interactable {
+
+    bool cinPlayed;
+
+	// Use this for initialization
+	void Start () {
+        cinPlayed = false;
+	}
+
+    public override void PlayerInteract()
+    {
+        GetComponent<TimelineController>().Play();
+    }
+}
---
+++ Assets/toyRoomScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toyRoomScript : Interactable {
+
+    public GameObject player;
+    private bool played;
+
+    private void Awake()
+    {
+        played = false;
+    }
+
+    public void doThis()
+    {
+        player.GetComponent<rpgStats>().Agility.Add(-1);
+        player.GetComponent<rpgStats>().Strength.Add(-1);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            if (!played)
+            {
+                GetComponent<TimelineController>().Play();
+                played = true;
+            }
+        }
+    }
+
+}
